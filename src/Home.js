2c0bc6251d2f13b1e525dd5006e7a4513c85da74
GeonLee <leegun2003@gmail.com>
2019-02-19 01:27:27 +0900

    refactored react-apollo width react-apollo-hooks

---
+++ src/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import { Helmet } from "react-helmet";
+import { HOME_PAGE } from "./queries";
+import { useQuery } from "react-apollo-hooks";
+import Movie from "./Movie";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 0.7fr);
+  flex-wrap: wrap;
+  justify-items: center;
+`;
+
+const Home = () => {
+  const { data, error, loading } = useQuery(HOME_PAGE);
+  return (
+    <Container>
+      <Helmet>
+        <title>Home | MovieQL</title>
+      </Helmet>
+      {loading && "Loading"}
+      {error && "Something is wrong"}
+      {data &&
+        data.movies.map(movie => (
+          <Movie
+            id={movie.id}
+            key={movie.id}
+            poster={movie.medium_cover_image}
+            title={movie.title}
+            rating={movie.rating}
+          />
+        ))}
+    </Container>
+  );
+};
+
+export default Home;
